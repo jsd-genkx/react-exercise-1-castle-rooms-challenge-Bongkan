7a03b56674831a86c057bcaf4a8bc11aa6cba0b5
Bongkan <nano159@hotmail.co.th>
2024-05-30 14:50:28 +0700

    Understood

--- src/components/02_Tower.jsx
+++ src/components/02_Tower.jsx
@@ -1,5 +1,14 @@
-const Tower = () => {
-  return <div>Tower</div>;
+import Chamber from "./03_Chamber";
+
+
+const Tower = ({ message1, onReply }) => {
+  return (
+    <div className="flex flex-col justify-center items-center pt-10">
+      <h1>Tower</h1>
+      <p>Message for JSD7: {message1}</p>
+      <Chamber message1={message1} onReply={onReply} />
+    </div>
+  );
 };
 
 export default Tower;
--- src/components/09_SecretRoom.jsx
+++ src/components/09_SecretRoom.jsx
@@ -1,5 +1,28 @@
-const SecretRoom = () => {
-  return <div>SecretRoom</div>;
+import { useState } from "react";
+
+const SecretRoom = ({ message1, onReply }) => {
+  const [reply, setReply] = useState("");
+
+  const handleSendReply = () => {
+    onReply(reply);
+  };
+  return (
+    <div className="flex flex-col justify-center items-center pt-10">
+      <h1>SecretRoom</h1>
+      <p>Message for JSD7: {message1}</p>
+      <textarea
+        className="border w-[200px]"
+        value={reply}
+        onChange={(e) => setReply(e.target.value)}
+        placeholder="Type your reply here..."
+      />
+      <button
+        onClick={handleSendReply}
+      >
+        Send Reply
+      </button>
+    </div>
+  );
 };
 
 export default SecretRoom;
